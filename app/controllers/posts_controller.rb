4eb4ef7b9fcb8227c694121a5a6be0582e53756e
xtrmdarc <diegoreyesco@hotmail.com>
2020-09-28 20:36:01 -0500

    Add controller actions to handle index, create and destroy post features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,26 @@
 class PostsController < ApplicationController
   def index
+    render json: Post.all, status: :ok
   end
 
-  def show
+  def create
+    new_post = Post.new posts_params
+
+    return render json: new_post, status: :ok if new_post.save
+    
+    render json: {errors: new_post.errors.full_messages}, status: :unprocessable_entity
+  
   end
 
   def destroy
+    post_to_destroy = Post.find(params[:id])
+    post_to_destroy.destroy
+    render json: post_to_destroy, status: :ok
+  end
+
+  private
+
+  def posts_params
+    params.permit(:name, :description)
   end
 end
